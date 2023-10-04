ef9eb350ab0aa191f65144f6fb0f83f9670fe213
ManuECyL <manuel.chipri@educa.jcyl.es>
2023-10-04 16:21:46 +0000

    index.php

--- Tema2/Tareas/03_ResumenTema2/index.php
+++ Tema2/Tareas/03_ResumenTema2/index.php
@@ -68,25 +68,27 @@
             </nav>
 
             <main>
-                <p>"1. Crea una página que:
-a. Muestra el nombre del fichero que se está ejecutando. 
-b. Muestra la dirección IP del equipo desde el que estás accediendo. 
-c. Muestra el path donde se encuentra el fichero que se está ejecutando. 
-d. Muestra la fecha y hora actual formateada en 2022-09-4 19:17:18. 
-e. Muestra la fecha y hora actual en Oporto formateada en (día de la semana, día de 
-mes de año, hh:mm:ss , Zona horaria). 
-f. Inicializa y muestra una variable en timestamp y en fecha con formato dd/mm/yyyy 
-de tu cumpleaños
-g. Calcular la fecha y el día de la semana de dentro de 60 días. 
-2. Crea una página a la que se le pase por url una variable con el nombre que quieras y 
-muestre el valor de la variable, el tipo, si es numérico o no y si lo es, si es entero o float. (No 
-hace falta usar if)
-3. Crea una página en la que se le pase como parámetros en la URL (ano, mes y día) y muestre 
-el día de la semana de dicho día. (Por defecto, dale el valor de 03/10/2023) 
-4. Crea una página en la que se le pase como parámetros en la URL (ano, mes y día) de dos 
-personas y muestre las fechas de nacimiento de ambos y la diferencia de edad en años. 
-5. Crea un enlace a una página en cada página anterior que muestre el contenido del fichero 
-que se está ejecutando."</p>
+                <p>
+                    1. Crea una página que:
+                        a. Muestra el nombre del fichero que se está ejecutando. 
+                        b. Muestra la dirección IP del equipo desde el que estás accediendo. 
+                        c. Muestra el path donde se encuentra el fichero que se está ejecutando. 
+                        d. Muestra la fecha y hora actual formateada en 2022-09-4 19:17:18. 
+                        e. Muestra la fecha y hora actual en Oporto formateada en (día de la semana, día de 
+                        mes de año, hh:mm:ss , Zona horaria). 
+                        f. Inicializa y muestra una variable en timestamp y en fecha con formato dd/mm/yyyy 
+                        de tu cumpleaños
+                        g. Calcular la fecha y el día de la semana de dentro de 60 días. 
+                    2. Crea una página a la que se le pase por url una variable con el nombre que quieras y 
+                        muestre el valor de la variable, el tipo, si es numérico o no y si lo es, si es entero o float. (No 
+                        hace falta usar if)
+                    3. Crea una página en la que se le pase como parámetros en la URL (ano, mes y día) y muestre 
+                        el día de la semana de dicho día. (Por defecto, dale el valor de 03/10/2023) 
+                    4. Crea una página en la que se le pase como parámetros en la URL (ano, mes y día) de dos 
+                        personas y muestre las fechas de nacimiento de ambos y la diferencia de edad en años. 
+                    5. Crea un enlace a una página en cada página anterior que muestre el contenido del fichero 
+                        que se está ejecutando."
+                </p>
             </main>
 
             <?php
