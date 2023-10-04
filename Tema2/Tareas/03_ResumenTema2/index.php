e352d024c8002b20984b0dc45174feb6972c7d42
ManuECyL <manuel.chipri@educa.jcyl.es>
2023-10-04 16:47:32 +0000

    Tarea03

--- Tema2/Tareas/03_ResumenTema2/index.php
+++ Tema2/Tareas/03_ResumenTema2/index.php
@@ -12,6 +12,12 @@
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
         
         <link rel="stylesheet" href="../../../css/estilos.css">
+
+        <style>
+            h6 {
+                font-weight: bold;
+            }
+        </style>
     </head>
 
     <body>
@@ -68,27 +74,34 @@
             </nav>
 
             <main>
-                <p>
-                    1. Crea una página que:
+                <pre>
+                    <h3 style="text-align: center">Enunciados de los ejercicios de la Tarea 03</h3>
+
+                    <h6>    1. Crea una página que:</h6>
+
                         a. Muestra el nombre del fichero que se está ejecutando. 
-                        b. Muestra la dirección IP del equipo desde el que estás accediendo. 
+                        <br>
+                        b. Muestra la dirección IP del equipo desde el que estás accediendo.
+                        <br>
                         c. Muestra el path donde se encuentra el fichero que se está ejecutando. 
+                        <br>
                         d. Muestra la fecha y hora actual formateada en 2022-09-4 19:17:18. 
-                        e. Muestra la fecha y hora actual en Oporto formateada en (día de la semana, día de 
-                        mes de año, hh:mm:ss , Zona horaria). 
-                        f. Inicializa y muestra una variable en timestamp y en fecha con formato dd/mm/yyyy 
-                        de tu cumpleaños
+                        <br>
+                        e. Muestra la fecha y hora actual en Oporto formateada en (día de la semana, día de mes de año, hh:mm:ss , Zona horaria). 
+                        <br>
+                        f. Inicializa y muestra una variable en timestamp y en fecha con formato dd/mm/yyyy de tu cumpleaños
+                        <br>
                         g. Calcular la fecha y el día de la semana de dentro de 60 días. 
-                    2. Crea una página a la que se le pase por url una variable con el nombre que quieras y 
-                        muestre el valor de la variable, el tipo, si es numérico o no y si lo es, si es entero o float. (No 
-                        hace falta usar if)
-                    3. Crea una página en la que se le pase como parámetros en la URL (ano, mes y día) y muestre 
-                        el día de la semana de dicho día. (Por defecto, dale el valor de 03/10/2023) 
-                    4. Crea una página en la que se le pase como parámetros en la URL (ano, mes y día) de dos 
-                        personas y muestre las fechas de nacimiento de ambos y la diferencia de edad en años. 
-                    5. Crea un enlace a una página en cada página anterior que muestre el contenido del fichero 
-                        que se está ejecutando."
-                </p>
+                        <br>
+
+                    <h6>    2. Crea una página a la que se le pase por url una variable con el nombre que quieras y muestre el valor de la variable, el tipo, si es numérico o no y si lo es, si es entero o float. (No hace falta usar if)</h6>
+
+                    <h6>    3. Crea una página en la que se le pase como parámetros en la URL (ano, mes y día) y muestre el día de la semana de dicho día. (Por defecto, dale el valor de 03/10/2023)</h6>
+
+                    <h6>    4. Crea una página en la que se le pase como parámetros en la URL (ano, mes y día) de dos personas y muestre las fechas de nacimiento de ambos y la diferencia de edad en años.</h6> 
+
+                    <h6>    5. Crea un enlace a una página en cada página anterior que muestre el contenido del fichero que se está ejecutando."</h6>
+                </pre>
             </main>
 
             <?php
