35faf82745cca1a1e72fd093ef0158cb48d0b83a
ManuECyL <manuel.chipri@educa.jcyl.es>
2023-11-09 18:54:24 +0000

    Tarea 10 - 2

--- Tema3/Tareas/10_ModificarFicheros/Ejercicio1/EligeFichero.php
+++ Tema3/Tareas/10_ModificarFicheros/Ejercicio1/EligeFichero.php
@@ -9,7 +9,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <title>Tarea 10 - Modificar Ficheros</title>
+        <title>Tarea 10 - 1 - EligeFichero.php</title>
 
     <!-- BOOTSTRAP -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
@@ -34,7 +34,6 @@
                 text-align:center;
                 margin: 40px;
             }
-
         </style>
     </head>
 
