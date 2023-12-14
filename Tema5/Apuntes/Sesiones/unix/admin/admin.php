7a53e8a5f7222a57de7a98d0e5528cb1c6e87c96
ManuECyL <manuel.chipri@educa.jcyl.es>
2023-12-14 19:38:58 +0000

    Tema 5

---
+++ Tema5/Apuntes/Sesiones/unix/admin/admin.php
@@ -0,0 +1,1 @@
+<h2>Esta pagina la ven solo los usuarios administradores</h2>
---
+++ Tema5/Apuntes/Sesiones/unix/paginaTodos.php
@@ -0,0 +1,3 @@
+<h2>Esta la ve todo el mundo</h2>
+<a href="./users/usuarios.php">Ir a Usuario</a>
+<a href="./admin/admin.php">Ir a Administrador</a>
