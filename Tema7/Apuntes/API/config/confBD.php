4d500dffc3a8ded440929eb040bcdd927e59035f
ManuECyL <manuel.chipri@educa.jcyl.es>
2024-02-02 12:39:42 +0000

    API Casa

--- Tema7/Apuntes/API/config/confBD.php
+++ Tema7/Apuntes/API/config/confBD.php
@@ -1,5 +1,6 @@
 <?php
-    define('IP', '192.168.7.207'); 
+    define('IP', '192.168.1.149'); // Casa
+    // define('IP', '192.168.7.207'); // Clase
     define('BBDD', 'api');
     define('USER', 'maria');
     define('PASS', 'maria');
--- Tema7/Apuntes/consumirApi/confApi.php
+++ Tema7/Apuntes/consumirApi/confApi.php
@@ -1,6 +1,7 @@
 <?php
 
     // URL
-    define("URI_API", "http://192.168.7.207/Tema7/Apuntes/API/index/");
+    // define("URI_API", "http://192.168.7.207/Tema7/Apuntes/API/index/"); // Clase
+    define("URI_API", "http://192.168.1.149/Tema7/Apuntes/API/index/"); // Casa
 
 ?>
